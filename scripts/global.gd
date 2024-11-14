f89382441d16e4e3557078454a5db57cceeb3e18
“Echo” <echorw927@gmail.com>
2024-11-13 20:02:48 -0800

    Create global.gd

---
+++ scripts/global.gd
@@ -0,0 +1,4 @@
+# This script can contain functions and variables that can be accessed in any other script.
+# To do that, you define it here and then in the other scripts you do Global.[thing]
+# you can pretty much put anything here that might be useful in other scripts
+
