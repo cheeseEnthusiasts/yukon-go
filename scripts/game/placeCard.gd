8c68160a0b27a9ee87ede1ada8a87fdd15ffc3c3
Xarrak-Dev <xarrak99@gmail.com>
2024-11-16 18:27:56 -0800

    fixed some of echos bs

--- scripts/game/placeCard.gd
+++ scripts/game/placeCard.gd
@@ -3,5 +3,7 @@ extends Node
 
 
 func _on_area_2d_input_event(viewport:Node, event:InputEvent, shape_idx:int) -> void:
-    if event is InputEventMouseButton and event.button_index == 1 and event.pressed == true:
-        $card.position = $cardspot.position
+	if event is InputEventMouseButton and event.button_index == 1 and event.pressed == true:
+		$"../cards/card".position = $"../cardspots/cardspot1".position
+		print("cardspot 1 clicked")
+		print(str($"../cards/card".position) + ", " + str($"../cardspots/cardspot1".position))
