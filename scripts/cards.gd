54ec97114c05edfd2cf2619b1b36a0f4cf3a694b
“Echo” <echorw927@gmail.com>
2024-11-19 20:27:27 -0800

    does this even help at all

--- scripts/cards.gd
+++ scripts/cards.gd
@@ -2,4 +2,5 @@ extends Node
 class_name Card
 
 func create(hp = 0, eng = 0, dmg = 0, card_art = "", ):
-    pass        
+   print('you called a useless function. congrats')
+   # i have no clue how this is going to work well
